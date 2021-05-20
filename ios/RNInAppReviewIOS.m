17d2314e0f76a146ac56dc5f0eb1191c33ca5cfc
Brad Anderson <banderso@nerdery.com>
2021-05-20 17:25:38 -0400

    #13 Adds support for non-deprecated requestReviewInScene to support ios 14.0 and above.

--- ios/RNInAppReviewIOS.m
+++ ios/RNInAppReviewIOS.m
@@ -21,7 +21,22 @@ RCT_EXPORT_MODULE()
 RCT_EXPORT_METHOD(requestReview:
     resolver:(RCTPromiseResolveBlock)resolve
     rejecter:(RCTPromiseRejectBlock)reject) {
-      if (@available(iOS 10.3, *)) {
+      // requestReview is deprecated in iOS 14.0+
+      // https://stackoverflow.com/a/63954318/1159534
+      if (@available(iOS 14.0, *)) {
+        UIWindowScene *activeScene;
+        NSSet* scenes = [[UIApplication sharedApplication] connectedScenes];
+        for (UIScene *scene in scenes) {
+            if ([scene activationState] == UISceneActivationStateForegroundActive) {
+                activeScene = (UIWindowScene *)scene;
+                break;
+            }
+        }
+        if (activeScene != nil) {
+          [SKStoreReviewController requestReviewInScene:activeScene];
+        }
+         resolve(@"true");
+      } else if (@available(iOS 10.3, *)) {
         [SKStoreReviewController requestReview];
          resolve(@"true");
       }else{
